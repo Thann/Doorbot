7811f4391119b92810488816a34db9543da892c6
Jonathan Knapp <jonathan@purse.io>
2018-02-08 23:26:18 -0800

    Fix tests with prefix

--- test/users.js
+++ test/users.js
@@ -2,7 +2,7 @@
 
 const db = require('../lib/db');
 const server = require('../server');
-const agent = require('supertest').agent(server);
+const agent = require('supertest').agent(server, {prefix: '/api/v1'});
 
 describe('Users API', function() {
 	before(async function() {
