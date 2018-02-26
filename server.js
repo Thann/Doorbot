1ab6382b354505cd84875a7248429793d8f84d7d
Jonathan Knapp <jonathan@purse.io>
2018-02-25 23:10:33 -0800

    Server: dev option restarts server on changes
    Deprecated watch option

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const path = require('path');
 const express = require('express');
 const expressWS = require('express-ws');
 const errors = require('./lib/errors');
-const app = express();
 
 const options = {
 	port: 3000,
@@ -18,9 +17,8 @@ if (require.main !== module) {
 } else {
 	const getopts = require('node-getopt').create([
 		['p', 'port=', 'Set listen port'],
-		['',  'watch', 'Recompile webapp on file modification'],
 		['',  'build', 'Compile webapp'],
-		['',  'dev',   'Watch and lint quickly with bigger bundles'],
+		['',  'dev',   'Compile webapp & restart server on file modification'],
 		['h', 'help'],
 	]).bindHelp().setHelp(
 		'Doorbot: server w/ webui to manage users and doors.\n' +
@@ -42,48 +40,63 @@ if (require.main !== module) {
 	Object.assign(options, opt.options);
 }
 
-// Load middleware
-app.use(require('body-parser').json());
-expressWS(app);
+if (options.dev) {
+	// Fork server
+	require('nodemon')({
+		script: 'server.js',
+		watch: ['api','lib'],
+		args: ['--port', options.port.toString()],
+		ext: 'js',
+	}).on('crash', () => {
+		process.exit(1);
+	}).on('quit', process.exit);
+} else {
+	// Init server
+	const app = express();
 
-// Serve static files
-app.use(express.static('dist', {setHeaders: function(res, path) {
-	if (path.endsWith('/bundle.js.gz')) {
-		res.setHeader('Content-Encoding', 'gzip');
-		res.setHeader('Content-Type', 'application/javascript');
-	}
-}}));
+	// Load middleware
+	app.use(require('body-parser').json());
+	expressWS(app);
 
-// Load all controllers from the api directory.
-const controllers = path.join(__dirname, 'api');
-const apiRouter = express.Router();
-app.use('/api/v1', apiRouter);
-fs.readdirSync(controllers).forEach(function(file) {
-	if (file.endsWith('.js'))
-		require(path.join(controllers, file))(apiRouter);
-});
+	// Serve static files
+	app.use(express.static('dist', {setHeaders: function(res, path) {
+		if (path.endsWith('/bundle.js.gz')) {
+			res.setHeader('Content-Encoding', 'gzip');
+			res.setHeader('Content-Type', 'application/javascript');
+		}
+	}}));
 
-// Handle errors
-app.use(function errorHandler(err, request, response, next) {
-	console.error('ERROR:', err);
-	next(err);
-});
+	// Load all controllers from the api directory.
+	const controllers = path.join(__dirname, 'api');
+	const apiRouter = express.Router();
+	app.use('/api/v1', apiRouter);
+	fs.readdirSync(controllers).forEach(function(file) {
+		if (file.endsWith('.js'))
+			require(path.join(controllers, file))(apiRouter);
+	});
 
-process.on('unhandledRejection', function(err, promise) {
-	if (!(err instanceof errors.HandledError))
-		console.error('UHR', err, promise);
-});
+	// Handle errors
+	app.use(function errorHandler(err, request, response, next) {
+		console.error('ERROR:', err);
+		next(err);
+	});
 
-module.exports = app.listen(options.port, function() {
-	if (require.main === module)
-		console.log('listening on', options.port);
-});
+	process.on('unhandledRejection', function(err, promise) {
+		if (!(err instanceof errors.HandledError))
+			console.error('UHR', err, promise);
+	});
 
-// --Watch
-if (options.watch || options.build || options.dev) {
+	// Start server
+	module.exports = app.listen(options.port, function() {
+		if (require.main === module)
+			console.log('listening on', options.port);
+	});
+}
+
+// Webpack watch
+if (options.build || options.dev) {
 	const opts = ['--color'];
-	if (options.dev) opts.push('--env.dev');
-	if (options.watch || options.dev) opts.push('--watch');
+	if (options.dev) opts.push('--watch', '--env.dev');
 	const watcher = require('child_process')
 		.spawn('node_modules/.bin/webpack', opts);
 
